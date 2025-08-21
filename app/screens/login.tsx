a2b1c2a43a30c78282166a80c70c39dab103c8ec
shiwani08 <shiwanisoni29082002@gmail.com>
2025-08-21 12:40:40 +0530

    Integrating auth apis

--- app/screens/login.tsx
+++ app/screens/login.tsx
@@ -1,71 +1,75 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Link, useRouter } from "expo-router";
 import React, { useState } from "react";
-import styles from "../styles/loginStyle"; 
+import styles from "../styles/loginStyle";
 import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
   ScrollView,
-  Alert
+  Alert,
 } from "react-native";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  const handleLogin = () => {
-  if (!email || !password) {
-    Alert.alert("Error", "Please enter email & password");
-    return;
-  }
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
 
-  Alert.alert(
-    "Logged in Successfully!!",
-    "Welcome back!",
-    [
-      {
-        text: "Continue",
-        onPress: () => router.replace("/"), 
-      },
-    ]
-  );
-};
+      const data = await response.json();
 
+      console.log("Login Response:", data);
+      alert(`Token: ${data.token}`);
+
+      if (response.ok) {
+        Alert.alert("Login Success", `Token: ${data.token}`, [
+          { text: "Continue", onPress: () => router.replace("/") },
+        ]);
+      } else {
+        Alert.alert("Login Failed", data.message || "Invalid credentials");
+      }
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Error", "Something went wrong");
+    }
+  };
 
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <View style={styles.container}>
-        <Text style={styles.title}>
-          <Ionicons name="person" style={styles.icon} size={20} />
-          Simplified Spaces
-        </Text>
+        <Text style={styles.title}>Simplified Spaces</Text>
 
-        <TextInput 
-          placeholder="Email" 
-          style={styles.input} 
-          value={email}
-          onChangeText={setEmail}
+        <TextInput
+          placeholder="Username"
+          style={styles.input}
+          value={username}
+          onChangeText={setUsername}
         />
-        <TextInput 
-          placeholder="Password" 
-          style={styles.input} 
-          secureTextEntry 
+        <TextInput
+          placeholder="Password"
+          style={styles.input}
+          secureTextEntry
           value={password}
           onChangeText={setPassword}
         />
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={handleLogin}
-        >
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
 
         <Text style={styles.text}>Not a user?</Text>
-        <Link href="/signup" style={styles.link}>
+        <Link href="./signup" style={styles.link}>
           Go to Sign Up
         </Link>
       </View>
